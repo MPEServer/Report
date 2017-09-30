c33a8f9fc43d86295c4dcf7be217af72c6f15669
expexes <devid.kohut@gmail.com>
2017-09-30 15:43:39 +0300

    Fix message template

--- src/main/java/tech/teslex/mpes/report/Report.java
+++ src/main/java/tech/teslex/mpes/report/Report.java
@@ -23,95 +23,91 @@ import java.util.Properties;
 
 public class Report extends PluginBase {
 
-	private static Config config;
+    private static Config config;
 
-	@Override
-	public void onEnable() {
-		getLogger().info("Enabled");
-		initConfig();
-		config = loadCfg();
+    @Override
+    public void onEnable() {
+        getLogger().info("Enabled");
+        initConfig();
+        config = loadCfg();
 
 //		if (config.getEmail().isUse())
 //			if (getServer().getPluginManager().getPlugin("MailApi") != null)
 //				getLogger().error("Plugin MailApi not found! Please install it and start again!");
-	}
+    }
 
-	private void initConfig() {
-		getDataFolder().mkdirs();
-		saveResource("config.json");
-	}
+    private void initConfig() {
+        getDataFolder().mkdirs();
+        saveResource("config.json");
+    }
 
-	private Config loadCfg() {
-		Gson gson = new Gson();
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new FileReader(getDataFolder() + "/config.json"));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return gson.fromJson(br, Config.class);
-	}
+    private Config loadCfg() {
+        Gson gson = new Gson();
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(getDataFolder() + "/config.json"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return gson.fromJson(br, Config.class);
+    }
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		switch (command.getLabel()) {
-			case "report":
-				if (args.length != 0) {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        switch (command.getLabel()) {
+            case "report":
+                if (args.length != 0) {
 
-					if (config == null)
-						return false;
+                    if (config == null)
+                        return false;
 
-					config.setMessageTemplate(config.getMessageTemplate()
-							.replace("{PLAYER}", sender.getName())
-							.replace("{TEXT}", String.join("+", args))
-							.replace(" ", sender.getName()));
+                    if (config.getVk().isUse()) {
+                        VKApi vkApi = new VKApiImpl();
 
-					if (config.getVk().isUse()) {
-						VKApi vkApi = new VKApiImpl();
+                        new Thread(() -> {
+                            try {
+                                vkApi.sendMessage(
+                                        String.valueOf(config.getVk().getReceiverId()),
+                                        config.getVk().getSenderToken(),
+                                        config.getMessageTemplate()
+                                                .replace(" ", "%20")
+                                                .replace("{TEXT}", String.join("%20", args))
+                                                .replace("{PLAYER}", sender.getName()));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }).start();
+                    }
 
-						new Thread(() -> {
-							try {
-								vkApi.sendMessage(
-										String.valueOf(config.getVk().getReceiverId()),
-										config.getVk().getSenderToken(),
-										config.getMessageTemplate()
-												.replace("{TEXT}", String.join(" ", args))
-												.replace("{PLAYER}", sender.getName()));
-							} catch (IOException e) {
-								e.printStackTrace();
-							}
-						}).start();
-					}
+                    if (config.getEmail().isUse()) {
+                        Properties p = new Properties();
+                        p.put("mail.smtp.auth", config.getEmail().getSmtpAuth());
+                        p.put("mail.smtp.starttls.enable", true);
+                        p.put("mail.smtp.host", config.getEmail().getSmtpHost());
+                        p.put("mail.smtp.port", config.getEmail().getSmtpPort());
+                        new Thread(() -> {
+                            try {
+                                new Mail(p, config.getEmail().getUsername(), config.getEmail().getPassword())
+                                        .send(
+                                                config.getEmail().getSenderEmail(),
+                                                config.getEmail().getReceiverMail(),
+                                                config.getEmail().getTitle(),
+                                                config.getMessageTemplate()
+                                                        .replace("{TEXT}", String.join(" ", args))
+                                                        .replace("{PLAYER}", sender.getName()),
+                                                config.getEmail().getType());
+                            } catch (MessagingException e) {
+                                e.printStackTrace();
+                            }
+                        }).start();
+                    }
 
-					if (config.getEmail().isUse()) {
-						Properties p = new Properties();
-						p.put("mail.smtp.auth", config.getEmail().getSmtpAuth());
-						p.put("mail.smtp.starttls.enable", true);
-						p.put("mail.smtp.host", config.getEmail().getSmtpHost());
-						p.put("mail.smtp.port", config.getEmail().getSmtpPort());
-						new Thread(() -> {
-							try {
-								new Mail(p, config.getEmail().getUsername(), config.getEmail().getPassword())
-										.send(
-												config.getEmail().getSenderEmail(),
-												config.getEmail().getReceiverMail(),
-												config.getEmail().getTitle(),
-												config.getMessageTemplate()
-														.replace("{TEXT}", String.join(" ", args))
-														.replace("{PLAYER}", sender.getName()),
-												config.getEmail().getType());
-							} catch (MessagingException e) {
-								e.printStackTrace();
-							}
-						}).start();
-					}
+                    sender.sendMessage(config.getOnSendMessage());
+                } else {
+                    sender.sendMessage("Error!");
+                }
 
-					sender.sendMessage(config.getOnSendMessage());
-				} else {
-					sender.sendMessage("Error!");
-				}
-
-		}
-		return true;
-	}
+        }
+        return true;
+    }
 }
